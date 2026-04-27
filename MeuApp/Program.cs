974baf268ae573d0e5a262e682f5ebfa6ca8b146
Felipe Fernandes <110199684+FelipeDantasFer@users.noreply.github.com>
2026-04-27 14:41:42 +0000

    corrigindo warning do ReadLine

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -11,14 +11,14 @@ namespace Course {
             y = new Triangulo();
 
             Console.WriteLine("Entre com as medidas do triangulo X:");
-            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.A = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
             Console.WriteLine("Entre com as medidas do triangulo Y:");
-            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.A = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
             double p = (x.A + x.B + x.C) / 2.0;
             double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
